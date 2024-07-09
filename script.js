989cfb7d2a24aa7ed71e883cefcaa80183c8c055
Emill03 <132957045+Emill03@users.noreply.github.com>
2024-07-09 15:26:07 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('darkModeToggle').addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    const buttonText = document.body.classList.contains('dark-mode') ? 'Modo Oscuro' : 'Modo Claro';
-    document.getElementById('darkModeToggle').textContent = buttonText;
-    alert(`Has cambiado a ${buttonText}`);
-});
